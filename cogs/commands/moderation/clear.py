cffa24dd2ed17e6a18c8dfb1ed6bef3859bdf224
Timéo L. <timeolourme07100@gmail.com>
2024-08-21 22:59:45 +0200

    Added clear system

---
+++ cogs/commands/moderation/clear.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+import modules.log as log
+
+import json; data = json.load(open('config.json', 'r', encoding='utf-8'))
+
+class clearCog(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    @app_commands.checks.has_any_role(*data['roles']['admin'])
+    @app_commands.command(name='clear', description='Clear a specified number of messages')
+    async def clear(self, interaction: discord.Interaction, amount: int):
+        await interaction.response.defer(ephemeral=False)
+        await interaction.channel.purge(limit=amount)
+        embed = discord.Embed(description=f'### {amount} messages have been cleared', colour=0x28b4d8)
+        await interaction.channel.send(embed=embed)
+
+    @clear.error
+    async def clear_error(self, interaction: discord.Interaction, error):
+        if isinstance(error, commands.MissingAnyRole):
+            embed = discord.Embed(description='### you do not have permission to use this command', colour=0x28b4d8)
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+        else:
+            embed = discord.Embed(description='### An error occurred', colour=0x28b4d8)
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+            log.write('cogs.commands.clear', error, log.levels.error)
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(clearCog(bot))
