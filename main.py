bfb3536fa2105be76ae7fd115540300f1d85b0de
Timéo L. <timeolourme07100@gmail.com>
2024-08-21 20:48:26 +0200

    Update language

--- main.py
+++ main.py
@@ -9,32 +9,32 @@ import modules.log as log
 async def console():
     while True:
         response = await asyncio.to_thread(input)
-        
+
         if response.startswith('load '):
             ext = response[5:]
             try:
                 await bot.load_extension(ext)
-                print(f'{ext} chargé')
+                print(f'{ext} charged')
             except Exception as e:
                 print(e)
-        elif response.startswith('unload '):
+        elif response.startswith('unload'):
             ext = response[7:]
             try:
                 await bot.unload_extension(ext)
-                print(f'{ext} déchargé')
+                print(f'{ext} dump')
             except Exception as e:
                 print(e)
         elif response.startswith('reload '):
             ext = response[7:]
             try:
                 await bot.reload_extension(ext)
-                print(f'{ext} rechargé')
+                print(f'{ext} recharged')
             except Exception as e:
                 print(e)
 
 
 class client(commands.Bot):
-    
+
     def __init__(self):
         super().__init__(
             command_prefix='$',
@@ -43,9 +43,9 @@ class client(commands.Bot):
         )
 
         self.initial_extensions = []
-        
+
         types = [dir for dir in os.listdir('cogs') if os.path.isdir(f'cogs/{dir}')]
-        
+
         for type in types:
             for root, _, files in os.walk(f"cogs/{type}"):
                 for file in files:
@@ -58,18 +58,18 @@ class client(commands.Bot):
 
 
     async def setup_hook(self):
-        log.write('main', 'Mode débug activé', log.levels.debug)
-        
+        log.write('main', 'Debug mode enabled', log.levels.debug)
+
         for ext in self.initial_extensions:
             await self.load_extension(ext)
-            log.write('main', f'{ext} chargé', log.levels.debug)
-        
+            log.write('main', f'{ext} charged', log.levels.debug)
+
         global synced
         synced = await bot.tree.sync()
-        
+
     async def on_ready(self):
         log.write('main', f'{self.user} is connected with {len(synced)} command(s) synchonizes(s) under version {data["version"]}', log.levels.info)
         await asyncio.create_task(console())
-    
+
 
 bot = client(); bot.run(data["token"])
