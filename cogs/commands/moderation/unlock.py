a662a15523d17de6543e2049b0f0346fdd82655f
Timéo L. <timeolourme07100@gmail.com>
2024-08-22 00:06:36 +0200

    Lock and Unlock optimisation

--- cogs/commands/moderation/unlock.py
+++ cogs/commands/moderation/unlock.py
@@ -6,32 +6,32 @@ import modules.log as log
 
 import json; data = json.load(open('config.json', 'r', encoding='utf-8'))
 
-class LockCog(commands.Cog):
+class UnlockCog(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
     @app_commands.checks.has_any_role(*data['roles']['admin'])
-    @app_commands.command(name='lock', description='Locks the current room')
-    async def lock(self, interaction:discord.Interaction):
+    @app_commands.command(name='unlock', description='Unlocks the current room')
+    async def unlock(self, interaction:discord.Interaction):
         await interaction.response.defer(thinking=False, ephemeral=True)
 
-        await interaction.channel.set_permissions(interaction.guild.default_role, send_messages=False)
+        await interaction.channel.set_permissions(interaction.guild.default_role, send_messages=True)
 
-        embed = discord.Embed(description='### The lounge has been successfully locked', colour=0x28b4d8)
+        embed = discord.Embed(description='### The lounge has been successfully unlocked', colour=0x28b4d8)
         await interaction.followup.send(embed=embed)
 
-        embed = discord.Embed(description='### The living room has been locked', colour=0x28b4d8)
+        embed = discord.Embed(description='### The living room has been unlocked', colour=0x28b4d8)
         await interaction.channel.send(embed=embed)
 
-    @lock.error
-    async def lock_error(self, interaction:discord.Interaction, error):
+    @unlock.error
+    async def unlock_error(self, interaction:discord.Interaction, error):
         if isinstance(error, commands.MissingAnyRole):
             embed = discord.Embed(description='### Do not have permission to use this command', colour=0x28b4d8)
             await interaction.response.send_message(embed=embed, ephemeral=True)
         else:
             embed = discord.Embed(description='### An error has occurred', colour=0x28b4d8)
             await interaction.response.send_message(embed=embed, ephemeral=True)
-            log.write('cogs.commands.lock', error, log.levels.error)
+            log.write('cogs.commands.unlock', error, log.levels.error)
 
 async def setup(bot: commands.Bot) -> None:
-    await bot.add_cog(LockCog(bot))
+    await bot.add_cog(UnlockCog(bot))
