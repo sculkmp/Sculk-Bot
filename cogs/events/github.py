80e79083bbb539a1546babaabb08b78eec0f51d1
Timéo L. <timeolourme07100@gmail.com>
2024-08-21 20:54:55 +0200

    Update optimisation github cogs

--- cogs/events/github.py
+++ cogs/events/github.py
@@ -3,46 +3,47 @@ from discord.ext import commands
 
 import requests
 import re
-import json; data = json.load(open('config.json', 'r', encoding='utf-8'))
+import json
 
-
+with open('config.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
 
 class Github(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+        self.github_url_pattern = re.compile(r'^https:\/\/github\.com\/sculkmp\/.*$')
 
-    def get_github_file(self, url:str) -> tuple[str, str]:
+    def get_github_file(self, url: str) -> tuple[str, str]:
+        """Fetches a GitHub file and returns its content and format"""
         url = url.replace('github.com', 'raw.githubusercontent.com').replace('/blob/', '/')
-
-        splitted_url = url.split('#L')
-        if len(splitted_url) != 2:
+        try:
+            url, line = url.split('#L')
+            line = int(line)
+        except ValueError:
             return ('Invalid URL', None)
-
-        url = splitted_url[0]
-        line = splitted_url[1]
 
         response = requests.get(url)
         if response.status_code not in [200, 201]:
             return ('Invalid URL', None)
 
         lines = response.text.split('\n')
-        start = max(0, int(line) - 5)
-        end = min(len(lines), int(line) + 5)
+        start, end = max(0, line - 5), min(len(lines), line + 5)
 
-        for i in range(start, end):
-            lines[i] = f' {i + 1} {lines[i]}'
+        formatted_lines = []
+        for i, line_content in enumerate(lines[start:end], start + 1):
+            if i == line:
+                formatted_lines.append(f'>{line_content}')
+            else:
+                formatted_lines.append(f'{i} {line_content}')
 
-        lines[int(line) - 1] = f'>{lines[int(line) - 1][1:]}'
-
-        return ('\n'.join(lines[start:end]), url.split('.')[-1])
+        return ('\n'.join(formatted_lines), url.split('.')[-1])
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message) -> None:
         if message.author.bot:
             return
 
-        pattern = re.compile(r'^https:\/\/github\.com\/sculkmp\/.*$')
-        if not pattern.match(message.content):
+        if not self.github_url_pattern.match(message.content):
             return
 
         content, format = self.get_github_file(message.content)
@@ -52,7 +53,6 @@ class Github(commands.Cog):
             return await message.reply(embed=embed)
 
         return await message.reply(f'```{format}\n{content}```')
-
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(Github(bot))
