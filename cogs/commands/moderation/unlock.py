8d3180251342be8678c203940c9d8a3dd2a47143
Timéo L. <timeolourme07100@gmail.com>
2024-08-22 14:28:03 +0200

    Added header sculk

--- cogs/commands/moderation/unlock.py
+++ cogs/commands/moderation/unlock.py
@@ -1,5 +1,19 @@
 import discord
 from discord.ext import commands
+#    ____             _ _              __  __ ____
+#   / ___|  ___ _   _| | | __         |  \/  |  _ \
+#   \___ \ / __| | | | | |/ /  _____  | |\/| | |_) |
+#    ___) | (__| |_| | |   <  |_____| | |  | |  __/
+#   |____/ \___|\__,_|_|_|\_\         |_|  |_|_|
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  @author: SculkTeams
+#  @link: http://www.sculkmp.org/
+
 from discord import app_commands
 
 import modules.log as log
